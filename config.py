c9399f7ec60ce41f549e8582b4bed9bacd6fdd3c
manas065 <itssmanas@gmail.com>
2026-04-05 19:58:01 +0530

    Removed old unused files

--- config.py
+++
@@ -1,9 +0,0 @@
-DATABASE_CONFIG = {
-    'dbname': 'story_nest',
-    'user': 'postgres',
-    'password': 'Manas@679',
-    'host': 'localhost',
-    'port': '5432'
-}
-
-SECRET_KEY = 'your-secret-key-change-this-in-production'
